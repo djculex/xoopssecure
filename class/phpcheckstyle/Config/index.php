a57f1d4579ea8c3a4bf774a957001955334a3c61
Michael Albertsen <culex@culex.dk>
2023-01-15 17:54:34 +0100

    phpCheckStyle class
    Tools for checking Coding Standards. Including config file for Xoops Standards

---
+++ class/phpcheckstyle/Config/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Index.php file.
+ *
+ * Using an index file in every folder not having any is one way of preventing directory browsing
+ * The best way is to remove the Indexes directive from your httpd.conf, this is however not always an option on for instance hosted server
+ */
+
+/**
+ * This index.php file will show an error '104 not found' when entering this folder
+ * and was created : 14-12-2022 23:24:54 by a xoopsSecure scan/create.
+
+ * @package   \XoopsModules\xoopssecure
+ * @copyright The XOOPS Project (https://xoops.org)
+ * @copyright 2022 Culex
+ * @author    Michael Albertsen (http://culex.dk) <culex@culex.dk>
+ * @link      https://github.com/XoopsModules25x/xoopssecure
+ * @since     1.0
+ */
+
+header('HTTP/1.0 404 Not Found');
---
+++ class/phpcheckstyle/NullReporter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace XoopsModules\Xoopssecure;
+
+/**
+ * Do not output anything.
+ */
+class NullReporter extends Reporter
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @param            Integer $line
+     *            the line number
+     * @param            String  $check
+     *            the name of the check
+     * @param            String  $message
+     *            the text to log
+     * @param            String  $level
+     *            the severity level
+     * @SuppressWarnings
+     */
+    public function writeError($line, $check, $message, $level = WARNING)
+    {
+        // Do nothing
+    }
+}
