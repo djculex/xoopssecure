5087e7175124f93e319e6dcf63b8588cb750a130
Michael Albertsen <culex@culex.dk>
2023-01-15 17:49:01 +0100

    Create runner.js

---
+++ assets/js/runner.js
@@ -0,0 +1,31 @@
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+/**
+ * Xoops xoopsSecure module for xoops
+ *
+ * @copyright 2021 XOOPS Project (https://xoops.org)
+ * @license   GPL 2.0 or later
+ * @package   xoopsSecure
+ * @since     1.0
+ * @min_xoops 2.5.11
+ * @author    Culex - Email:culex@culex.dk - Website:https://www.culex.dk
+ */
+$(document).ready(
+    function () {
+
+        function xoopssecure_timeforcron(bValue)
+        {
+            if (bValue == true) {
+                $.getJSON(xoopsSecureSysUrl + 'agent.php?type=test?id=' + i, (data) => {});
+            }
+        }
+
+    }
+);
