99102dc39c1bca22ec97d3de38f8ba19f5778714
Michael Albertsen <culex@culex.dk>
2023-01-15 17:57:10 +0100

    Create xoops_version.inc.php

---
+++ include/xoops_version.inc.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Xoops XoopsSecure module for xoops
+ *
+ * @copyright 2021 XOOPS Project (https://xoops.org)
+ * @license   GPL 2.0 or later
+ * @package   Xoopssecure
+ * @since     1.0
+ * @min_xoops 2.5.11
+ * @author    Culex - Email:culex@culex.dk - Website:https://www.culex.dk
+ */
+
+/** Return int byte from String
+ * @param  $val
+ * @return float|int
+ */
+function xoopssecureReturnBytes($val)
+{
+    switch (\mb_substr($val, -1)) {
+        case 'K':
+        case 'k':
+            return (int)$val * 1024;
+        case 'M':
+        case 'm':
+            return (int)$val * 1048576;
+        case 'G':
+        case 'g':
+            return (int)$val * 1073741824;
+        default:
+            return $val;
+    }
+}
