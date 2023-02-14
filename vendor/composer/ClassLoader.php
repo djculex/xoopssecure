0b872688ed60c06c36ac735316b0516bd941c30e
Michael Albertsen <culex@culex.dk>
2023-02-14 18:49:51 +0100

    spell check

--- vendor/composer/ClassLoader.php
+++ vendor/composer/ClassLoader.php
@@ -27,13 +27,13 @@ use InvalidArgumentException;
  *     // activate the autoloader
  *     $loader->register();
  *
- *     // to enable searching the include path (eg. for PEAR packages)
+ *     // to enable searching to include path (e.g. for PEAR packages)
  *     $loader->setUseIncludePath(true);
  *
  * In this example, if you try to use a class in the Symfony\Component
  * namespace or one of its children (Symfony\Component\Console for instance),
  * the autoloader will first look for the class under the component/
- * directory, and it will then fallback to the framework/ directory if not
+ * directory, and it will then fall back to the framework/ directory if not
  * found before giving up.
  *
  * This class is loosely based on the Symfony UniversalClassLoader.
