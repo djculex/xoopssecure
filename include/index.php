ab8893c81c813c8fec1f44ba3d83c5fa814fad78
Michael Albertsen <culex@culex.dk>
2023-02-24 16:27:42 +0100

    - adjusted css width of container in malware scan result - refresh of index.php files

--- include/index.php
+++ include/index.php
@@ -1,15 +1,24 @@
 <?php
-declare(strict_types=1);
-
 /**
- * Xoops XoopsSecure module for xoops
- *
- * @copyright 2021 XOOPS Project (https://xoops.org)
- * @license   GPL 2.0 or later
- * @package   Xoopssecure
- * @since     1.0
- * @min_xoops 2.5.11
- * @author    Culex - Email:culex@culex.dk - Website:https://www.culex.dk
+ * Index.php file.
+ * 
+ * Using an index file in every folder not having any is one way of preventing directory browsing
+ * The best way is to remove the Indexes directive from your httpd.conf, 
+this is however not always an option on for instance hosted server 
+ * 
  */
 
-header('HTTP/1.0 404 Not Found');
+
+/**
+  * This index.php file will show an error '404 not found' when entering this folder
+  * and was created : 24-02-2023 16:23:01 by a xoopsSecure scan/create.
+
+  * @package      \XoopsModules\xoopssecure
+  * @copyright    The XOOPS Project (https://xoops.org)
+  * @copyright    2023 Culex
+  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
+  * @link         https://github.com/XoopsModules25x/xoopssecure
+  * @since        1.0
+  */
+
+header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found", true, 404);
--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <?php
-
 /**
  * Index.php file.
- *
+ * 
  * Using an index file in every folder not having any is one way of preventing directory browsing
- * The best way is to remove the Indexes directive from your httpd.conf, this is however not always an option on for instance hosted server
+ * The best way is to remove the Indexes directive from your httpd.conf, 
+this is however not always an option on for instance hosted server 
+ * 
  */
+
 
 /**
- * This index.php file will show an error '104 not found' when entering this folder
- * and was created : 15-12-2022 09:12:26 by a xoopsSecure scan/create.
- * @package   \XoopsModules\xoopssecure
- * @copyright The XOOPS Project (https://xoops.org)
- * @copyright 2022 Culex
- * @author    Michael Albertsen (http://culex.dk) <culex@culex.dk>
- * @link      https://github.com/XoopsModules25x/xoopssecure
- * @since     1.0
- */
+  * This index.php file will show an error '404 not found' when entering this folder
+  * and was created : 24-02-2023 16:22:59 by a xoopsSecure scan/create.
 
-header('HTTP/1.0 404 Not Found');
+  * @package      \XoopsModules\xoopssecure
+  * @copyright    The XOOPS Project (https://xoops.org)
+  * @copyright    2023 Culex
+  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
+  * @link         https://github.com/XoopsModules25x/xoopssecure
+  * @since        1.0
+  */
+
+header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found", true, 404);
